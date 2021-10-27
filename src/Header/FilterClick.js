c9348dfa278aee322a55d9c021b1bcfc0c6928f9
akshay-augustine-sheby <akshay.sheby@bigbinary.com>
2021-10-27 16:21:22 +0530

    added more functionality to filter

--- src/Header/FilterClick.js
+++ src/Header/FilterClick.js
@@ -1,7 +1,7 @@
-import React,{useState} from "react"
+import React,{useEffect, useState} from "react"
 import { Pane, Typography, Button,Checkbox } from "@bigbinary/neetoui/v2";
-import { Filter } from "@bigbinary/neeto-icons";
-import Check from "./Check";
+import { Check } from "@bigbinary/neeto-icons";
+import Check2 from "./Check";
 const FilterClick= ({
     showPane,
     setShowPane,
@@ -14,6 +14,26 @@ const FilterClick= ({
     checkSports,
     setCheckSports
 }) => {
+    
+    useEffect(()=>{
+        setCheckNational2(checkNational)
+        setCheckWorld2(checkWorld)
+        setCheckBusiness2(checkBusiness)
+        setCheckSports2(checkSports)
+    },[showPane])
+
+    const [checkNational2,setCheckNational2] = useState(true)
+    const [checkWorld2,setCheckWorld2] = useState(true)
+    const [checkBusiness2, setCheckBusiness2] = useState(true)
+    const [checkSports2, setCheckSports2] = useState(true)
+
+    const handleSave=()=>{
+        setShowPane(false)
+        setCheckNational(checkNational2)
+        setCheckWorld(checkWorld2)
+        setCheckBusiness(checkBusiness2)
+        setCheckSports(checkSports2)
+    }
     
     return (
       <div className="w-full">
@@ -29,43 +49,35 @@ const FilterClick= ({
             <Typography style="body2">
             Category
             </Typography>
-            <Check    
+            <Check2    
                 name="National"
-                checked={checkNational}
-                onChange={e => {
-                    setCheckNational(e.target.checked)
-                }
-                } 
-            />
-            
-            <Check    
+                checked={checkNational2}
+                onChange={e => setCheckNational2(e.target.checked)} 
+            />           
+            <Check2    
                 name="World"
-                checked={checkWorld}
-                onChange={e => setCheckWorld(e.target.checked)} 
+                checked={checkWorld2}
+                onChange={e => setCheckWorld2(e.target.checked)} 
             />
-            <Check    
+            <Check2    
                 name="Business"
-                checked={checkBusiness}
-                onChange={e => setCheckBusiness(e.target.checked)} 
+                checked={checkBusiness2}
+                onChange={e => setCheckBusiness2(e.target.checked)} 
             />
-            <Check    
+            <Check2    
                 name="Sports"
-                checked={checkSports}
-                onChange={e => setCheckSports(e.target.checked)} 
+                checked={checkSports2}
+                onChange={e => setCheckSports2(e.target.checked)} 
             />
           </Pane.Body>
           <Pane.Footer className="flex items-center space-x-2">
-            <Checkbox
-                checked
-                id="checkbox_name"
-                label="Trial"
-                
-            />
             <Button
-              icon={Check}
-              size="large"
-              label="Continue"
-              onClick={() => setShowPane(false)}
+                iconPosition="right"
+                size="large"
+                label="Save Changes"
+                icon={Check}
+                onClick={handleSave}
+                style="primary"
             />
             <Button
               style="text"
