ccb4fca91e0e2ac75f4e5ed6d00ad4b2a28794ba
akshay-augustine-sheby <akshay.sheby@bigbinary.com>
2021-11-01 02:04:25 +0530

    added error boundaries

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -9,21 +9,17 @@ class ErrorBoundary extends React.Component {
       this.state = { hasError: false };
       
     }
-    
-  
+
     static getDerivedStateFromError(error) {
-      // Update state so the next render will show the fallback UI.
       return { hasError: true };
     }
   
     componentDidCatch(error, errorInfo) {
-      // You can also log the error to an error reporting service
       console.log(error);
     }
     
     render() {
       if (this.state.hasError) {
-        // You can render any custom fallback UI
         return (
             <div className="mt-52">
                 <img className="object-none h-56 w-full" src={img} alt="No image" />
